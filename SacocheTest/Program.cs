d43c9e755909c5dd0ac1661494343c7fa179d507
Richer Archambault <richer.arc@gmail.com>
2018-01-26 15:49:12 -0500

    all

---
+++ SacocheTest/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Net;
+using Netwrk.Web;
+
+namespace SacocheTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var webListener = new NetwrkWebListener(12345);
+
+            webListener.OnWebSocketConnection += (listener, socket) =>
+                {
+                    socket.OnBinaryMessage += (webSocket, data) =>
+                    {
+                        webSocket.Send(data);
+                    };
+                };
+            bool started = webListener.Start();
+
+            Console.ReadLine();
+
+        }
+    }
+}
